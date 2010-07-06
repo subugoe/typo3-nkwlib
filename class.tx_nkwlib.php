59a1714094b190dff5048706d813c75cc852be03
windisch <windisch@sub.uni-goettingen.de>
2010-07-06 07:52:42 +0000

    Code Cleaning ext:nkwlib jira-www-154
    git-svn-id: https://develop.sub.uni-goettingen.de/repos/w3d/branches/dev/typo3conf/ext/nkwlib@467 f004c202-0a6e-0410-85e4-8d7485fa9ffe

--- class.tx_nkwlib.php
+++ class.tx_nkwlib.php
@@ -13,7 +13,7 @@ class tx_nkwlib extends tslib_pibase {
 	}
 
 	function geocodeAddress($str) {
-		$str = ereg_replace(" ", "+", $str);
+		$str = ereg_replace(' ', '+', $str);
 		$getThis = "http://maps.google.com/maps/api/geocode/json?address=" . $str . "&sensor=false";
 		$json = file_get_contents($getThis);
 		$tmp = json_decode($json, true);
@@ -24,14 +24,14 @@ class tx_nkwlib extends tslib_pibase {
 	function getPageUrl($clean = FALSE) {
 		$url = $GLOBALS['TSFE']->baseUrl . $GLOBALS['TSFE']->anchorPrefix;
 		if ($clean) {
-			$tmp = explode("?",$url);
-			$url = $tmp["0"];
+			$tmp = explode('?',$url);
+			$url = $tmp[0];
 		}
 		return $url;
 	}
 
 	function getLanguage() {
-		$lang = $GLOBALS["TSFE"]->sys_page->sys_language_uid;
+		$lang = $GLOBALS['TSFE']->sys_page->sys_language_uid;
 		return $lang;
 	}
 
@@ -50,27 +50,27 @@ class tx_nkwlib extends tslib_pibase {
 
 		$pageInfo = $this->pageInfo($id, $lang);
 
-		if (!empty($pageInfo["tx_nkwkeywords_keywords"])) {
+		if (!empty($pageInfo['tx_nkwkeywords_keywords'])) {
 			if ($mode == "header") {
-				$tmp = explode(",", $pageInfo["tx_nkwkeywords_keywords"]);
+				$tmp = explode(",", $pageInfo['tx_nkwkeywords_keywords']);
 				foreach($tmp AS $key => $value) {
 					$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-							"*", "tx_nkwkeywords_keywords", "uid = '" . $value . "'", "", "", "");
+							'*', 'tx_nkwkeywords_keywords', "uid = '" . $value . "'", '', '', '');
 					while($row1 = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res1))
-						$tmpList .= $row1["title".$sep] . ",";
+						$tmpList .= $row1['title' . $sep] . ',';
 				}
 				$str .= substr($tmpList, 0, -1);
-			} else if ($mode == "infobox") {
-				$tmp = explode(",", $pageInfo["tx_nkwkeywords_keywords"]);
+			} else if ($mode == 'infobox') {
+				$tmp = explode(',', $pageInfo['tx_nkwkeywords_keywords']);
 				foreach($tmp AS $key => $value) {
 					$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-							"*", "tx_nkwkeywords_keywords", "uid = '" . $value . "'", "", "", "");
+							'*', 'tx_nkwkeywords_keywords', "uid = '" . $value . "'", '', '', '');
 					while($row1 = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res1)) {
-						$str .= "<li>" . $this->pi_LinkToPage(
-								$row1["title".$sep], 
-								$GLOBALS['TSFE']->tmpl->flatSetup["keywordslandingpage"], 
-								"", 
-								array("tx_nkwkeywords[id]" => $value)) . "</li>";
+						$str .= '<li>' . $this->pi_LinkToPage(
+								$row1['title' . $sep], 
+								$GLOBALS['TSFE']->tmpl->flatSetup['keywordslandingpage'], 
+								'', 
+								array('tx_nkwkeywords[id]' => $value)) . '</li>';
 					}
 				}
 			}
@@ -84,25 +84,25 @@ class tx_nkwlib extends tslib_pibase {
 
 		if ($lang > 0) {
 			$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-				"*", "pages_language_overlay", "pid = '" . $id . "'", "", "", "");
+				'*', 'pages_language_overlay', "pid = '" . $id . "'", '', '', '');
 		} else {
-			$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery("*", "pages", "uid = '" . $id . "'", "", "", "");
+			$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'pages', "uid = '" . $id . "'", '', '', '');
 		}
 
 		while($row1 = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res1)) {
-			$pageInfo["uid"] = $row1["uid"];
-			$pageInfo["pid"] = $row1["pid"];
-			$pageInfo["title"] = $row1["title"];
-			$pageInfo["keywords"] = $row1["keywords"];
-			$pageInfo["tx_nkwsubmenu_knot"] = $row1["tx_nkwsubmenu_knot"];
-			$pageInfo["tx_nkwkeywords_keywords"] = $row1["tx_nkwkeywords_keywords"];
+			$pageInfo['uid'] = $row1['uid'];
+			$pageInfo['pid'] = $row1['pid'];
+			$pageInfo['title'] = $row1['title'];
+			$pageInfo['keywords'] = $row1['keywords'];
+			$pageInfo['tx_nkwsubmenu_knot'] = $row1['tx_nkwsubmenu_knot'];
+			$pageInfo['tx_nkwkeywords_keywords'] = $row1['tx_nkwkeywords_keywords'];
 		}
 
 		if ($lang > 0) {
 			$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-					"tx_nkwkeywords_keywords", "pages", "uid = '" . $pageInfo["pid"] . "'", "", "", "");
+					'tx_nkwkeywords_keywords', 'pages', "uid = '" . $pageInfo["pid"] . "'", '', '', '');
 			while($row1 = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res1)) {
-				$pageInfo["tx_nkwkeywords_keywords"] = $row1["tx_nkwkeywords_keywords"];
+				$pageInfo['tx_nkwkeywords_keywords'] = $row1['tx_nkwkeywords_keywords'];
 			}
 		}
 
@@ -112,12 +112,12 @@ class tx_nkwlib extends tslib_pibase {
 
 	function knotID($id) {
 		$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-			"uid,pid,tx_nkwsubmenu_knot", "pages", "uid = '" . $id . "'", "", "", "");
+			'uid, pid, tx_nkwsubmenu_knot', 'pages', "uid = '" . $id . "'", '', '', '');
 		while($row1 = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res1)) {
-			if ($row1["tx_nkwsubmenu_knot"]) {
-				return $row1["uid"];
-			} else if ($row1["pid"] != 3) {
-				return $this->knotID($row1["pid"]);
+			if ($row1['tx_nkwsubmenu_knot']) {
+				return $row1['uid'];
+			} else if ($row1['pid'] != 3) {
+				return $this->knotID($row1['pid']);
 			}
 		}
 	}
@@ -135,10 +135,10 @@ class tx_nkwlib extends tslib_pibase {
 				"*","pages","pid = '".$id."' AND deleted = '0' AND hidden = '0' AND sys_language_uid = '0'","","sorting ASC","");
 */
 		$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-				"*", "pages", "pid = '" . $id . "' AND deleted = '0' AND hidden = '0'", "", "sorting ASC", "");
+				'*', 'pages', "pid = '" . $id . "' AND deleted = '0' AND hidden = '0'", '', 'sorting ASC', '');
 		while($row1 = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res1)) {
-			$arr[$i]["uid"] = $row1["uid"];
-			$arr[$i]["title"] = $row1["title"];
+			$arr[$i]['uid'] = $row1['uid'];
+			$arr[$i]['title'] = $row1['title'];
 			$i++;
 		}
 		if ($i > 0) {
@@ -164,9 +164,9 @@ class tx_nkwlib extends tslib_pibase {
 	// check if a page uses the content of another page "content_from_pid"
 	function checkForAlienContent($id) {
 		$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-				"uid,content_from_pid", "pages", "uid = '" . $id . "'", "", "", "");
+				'uid, content_from_pid', 'pages', "uid = '" . $id . "'", '', '', '');
 		while($row1 = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res1)) {
-			$content_from_pid = $row1["content_from_pid"];
+			$content_from_pid = $row1['content_from_pid'];
 		}
 		if ($content_from_pid) {
 			$id = $content_from_pid;
@@ -181,25 +181,25 @@ class tx_nkwlib extends tslib_pibase {
 		$arr = array();
 		if ($lang > 0) {
 			$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-				"uid,header", 
-				"tt_content", 
+				'uid, header', 
+				'tt_content', 
 				"pid = '" . $id . "' AND deleted = '0' AND hidden = '0' AND sys_language_uid = '" . $lang 
 					. "' AND t3ver_wsid != '-1'", 
-				"", 
-				"sorting ASC", 
-				"");
+				'', 
+				'sorting ASC', 
+				'');
 		} else {
 			$res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
-			"uid,header", 
-			"tt_content", 
+			'uid, header', 
+			'tt_content', 
 			"pid = '" . $id . "' AND deleted = '0' AND hidden = '0' AND  sys_language_uid = '0' AND t3ver_wsid != '-1'", 
-			"", 
-			"sorting ASC", 
-			"");
+			'', 
+			'sorting ASC', 
+			'');
 		}
 		while($row1 = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res1)) {
-			$arr[$i]["uid"] = $row1["uid"];
-			$arr[$i]["header"] = $row1["header"];
+			$arr[$i]['uid'] = $row1['uid'];
+			$arr[$i]['header'] = $row1['header'];
 			$i++;
 		}
 		if ($i > 0) {
@@ -212,7 +212,7 @@ class tx_nkwlib extends tslib_pibase {
 
 	function pageKeywordsList($id, $lang = FALSE) {
 		$pageInfo = $this->pageInfo($id, $lang);
-		$keywords = explode(",", $pageInfo["keywords"]);
+		$keywords = explode(',', $pageInfo['keywords']);
 		if (is_array($keywords)) {
 			return $keywords;
 		} else {
@@ -234,41 +234,41 @@ function isChild()
 */
 
 	function formatString($str) {
-		$str = ereg_replace("&", "&amp;", $str);
+		$str = ereg_replace('&', '&amp;', $str);
 		return $str;
 	}
 
 	# retruns todays unix time stamp (day start)
 	function hTime() {
-		$time = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+		$time = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
 		return $time;
 	}
 
 	# returns a humanreadable date format
 	function hReturnFormatDate($time, $lang = FALSE) {
-		$date = date("d", $time) . "." . date("m", $time) . "." . date("Y", $time);
+		$date = date('d', $time) . '.' . date('m', $time) . '.' . date('Y', $time);
 		if ($lang != 0) {
-			$date = date("Y", $time) . "-" . date("m", $time) . "-".date("d", $time);
+			$date = date('Y', $time) . '-' . date('m', $time) . '-' . date('d', $time);
 		}
 		return $date;
 	}
 	
 	function hReturnFormatDateSortable($time) {
-		$date = date("Y", $time) . "-" . date("m", $time) . "-" . date("d", $time);
+		$date = date('Y', $time) . '-' . date('m', $time) . '-' . date('d', $time);
 		return $date;
 	}	
 
 	function getPluginConf($pluginName) {
-		$pluginName .= ".";
+		$pluginName .= '.';
 		$array = $GLOBALS['TSFE']->tmpl->setup['plugin.'][$pluginName];
 		return $array;
 	}
 
 	# debug output
 	function dPrint($str) {
-		echo "<pre>";
+		echo '<pre>';
 		print_r($str);
-		echo "</pre>";
+		echo '</pre>';
 	}
 
 
