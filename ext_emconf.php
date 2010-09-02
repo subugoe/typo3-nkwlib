acf80de13a45ecca9e2652729095e1d1e30759bc
windisch <windisch@sub.uni-goettingen.de>
2010-09-02 08:44:57 +0000

    git-svn-id: https://develop.sub.uni-goettingen.de/repos/w3d/branches/dev/typo3conf/ext/nkwlib@614 f004c202-0a6e-0410-85e4-8d7485fa9ffe

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,26 +1,15 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2010 Nils K. Windisch <windisch@sub.uni-goettingen.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+
+########################################################################
+# Extension Manager/Repository config file for ext "nkwlib".
+#
+# Auto generated 31-08-2010 13:21
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'NKW Library',
 	'description' => '',
@@ -40,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Goettingen State and University Library, Germany http://www.sub.uni-goettingen,de',
-	'version' => '0.0.23',
+	'version' => '0.0.24',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -49,6 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"ed0a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"d9d5";s:20:"doc/wizard_form.html";s:4:"ed0e";}',
+	'_md5_values_when_last_written' => 'a:4:{s:10:"README.txt";s:4:"719c";s:19:"class.tx_nkwlib.php";s:4:"b844";s:12:"ext_icon.gif";s:4:"c8ca";s:16:"locallang_db.xml";s:4:"a9f8";}',
+	'suggests' => array(
+	),
 );
+
 ?>
